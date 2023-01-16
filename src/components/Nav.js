ece0ff24bbba17670d8877988aa0e4fa01b25f1c
minha0817 <minhakk24@gmail.com>
2023-01-16 10:42:28 -0800

    First commit

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Nav() {
+  return(
+    <nav>
+      <div>
+        <span>Minha</span>
+      </div>
+      <div>
+        <span>Home</span>
+        <span>About me</span>
+        <span>Skills</span>
+        <span>My work</span>
+        <span>Contact</span>
+      </div>
+    </nav>
+  )
+}
