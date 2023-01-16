96526785b3dbb5f4329f019259ab2a30f3d3babd
minha0817 <minhakk24@gmail.com>
2023-01-16 11:15:37 -0800

    Update navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,16 +2,19 @@ import React from "react";
 
 export default function Nav() {
   return(
-    <nav>
-      <div>
-        <span>Minha</span>
+    <nav id="navbar">
+      <div className="navbar__logo">
+      <a href="/">Minha</a>
       </div>
       <div>
-        <span>Home</span>
-        <span>About me</span>
-        <span>Skills</span>
-        <span>My work</span>
-        <span>Contact</span>
+        <ul className="navbar__menu">
+          <li className="navbar__menu__item">Home</li>
+          <li className="navbar__menu__item">About me</li>
+          <li className="navbar__menu__item">Skiils</li>
+          <li className="navbar__menu__item">My Work</li>
+          <li className="navbar__menu__item">Contact</li>
+        </ul>
+
       </div>
     </nav>
   )
