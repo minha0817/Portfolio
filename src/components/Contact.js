1f6ba7925c0db34918195dc675da390ffc6713e9
minha0817 <minhakk24@gmail.com>
2023-01-17 10:44:55 -0800

    Add Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { AiFillGithub, AiFillLinkedin, AiFillMail } from "react-icons/ai";
+import { GrDocumentText } from "react-icons/gr";
 
 export default function Contact() {
   return (
-    <section>Contact</section>
-  )
+    <section id="contact">
+      <h1 className="contact__title">Let's talk</h1>
+      <div className="contact__links">
+        <a href="https://github.com/minha0817" target="_blank">
+          <AiFillGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/minhakim24/" target="_blank">
+          <AiFillLinkedin />
+        </a>
+        <a href="mailto:minhakk24@gmail.com">
+          <AiFillMail />
+        </a>
+        <a href="https://docs.google.com/document/d/1WAUMjDtaMMa-snXnXRmsjrN1uYe30r_p/edit" target="_blank">
+          <GrDocumentText />
+        </a>
+      </div>
+    </section>
+  );
 }
