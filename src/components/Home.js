f867a3a96349f209dd5e250b60a953c175c01828
minha0817 <minhakk24@gmail.com>
2023-01-17 20:10:18 -0800

    Add styling for Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Picture from "../pics/minha.JPG";
+import Picture from "../pics/profile2.png";
+import "../styles/home.css";
 
 export default function Home() {
   return(
     <section id="home">
-      <img className="home__picture" style={{width: 120, height: 150}} src={Picture} alt="Picture"></img>
+      <img className="home__picture" src={Picture} alt="Picture"></img>
       <h1 className="home__title">Hello, <br/>I'm Minha Kim</h1>
-      <h2 className="home__description">A Front-end Developer currently residing in Vancouver, Canada </h2>
-      <button className="home__contact">Contact me</button>
+      <h3 className="home__description">A Front-end Developer currently residing in Vancouver, Canada </h3>
+      <button className="home__contact">CONTACT ME</button>
     </section>
   )
 }
