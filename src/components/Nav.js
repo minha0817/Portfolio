6e45bede834db67499712c96d41a235200e646dc
minha0817 <minhakk24@gmail.com>
2023-01-17 18:03:13 -0800

    Add styling for Navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,21 @@
 import React from "react";
 import icon from "../pics/favicon2.png";
+import "../styles/nav.css";
 
 export default function Nav() {
+
   return (
     <nav id="navbar">
-      <div className="navbar__logo">
-        <a href="/">
-          <img src={icon} style={{ width: 20, height: 20 }} alt="icon"></img>
-          Minha
+      <div>
+        <a href="/" className="navbar__logo">
+          <img src={icon} alt="icon" className="navbar__logo__icon"></img>
         </a>
       </div>
       <div>
         <ul className="navbar__menu">
-          <li className="navbar__menu__item">Home</li>
+          <li className="navbar__menu__item active" >Home</li>
           <li className="navbar__menu__item">About me</li>
-          <li className="navbar__menu__item">Skiils</li>
+          <li className="navbar__menu__item">Skills</li>
           <li className="navbar__menu__item">My Work</li>
           <li className="navbar__menu__item">Contact</li>
         </ul>
