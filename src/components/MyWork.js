622260af1070e9ba3dc704ed4b20d6a55cb5de5b
minha0817 <minhakk24@gmail.com>
2023-01-18 15:06:06 -0800

    Add styling for Mywork

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -2,6 +2,7 @@ import React from "react";
 import fithub from "../pics/projects/fithub.png";
 import pickmiup from "../pics/projects/pickmiup.png";
 import todolist from "../pics/projects/todolist.png";
+import "../styles/mywork.css";
 
 export default function MyWork() {
   return (
@@ -16,26 +17,53 @@ export default function MyWork() {
           Completed<span className="mywork__count">2</span>
         </button>
         <button className="mywork__button">
-          In Process<span className="mywork__count">1</span>
+          In Progress<span className="mywork__count">1</span>
         </button>
       </div>
       <div className="projects">
-        <a href="https://github.com/minha0817/workout_tracker" className="project"  target="_blank">
-          <img className="project__img" style={{width: 200, height: 150}} src={fithub} alt="Project Fithub"></img>
+        <a
+          href="https://github.com/minha0817/workout_tracker"
+          target="_blank"
+          className="project"
+        >
+          <img
+            className="project__img"
+            src={fithub}
+            alt="Project Fithub"
+          ></img>
           <div className="project__description">
             <h3>Workout Tracker, Fithub</h3>
             <span>HTML, CSS, JS, React, NodeJS, Express, PostSQL, MUI</span>
           </div>
         </a>
-        <a href="https://github.com/nicohsfu/pikmiup" className="project"  target="_blank">
-          <img className="project__img" style={{width: 200, height: 150}} src={pickmiup} alt="Project Pickmiup"></img>
+        <a
+          href="https://github.com/nicohsfu/pikmiup"
+          className="project"
+          target="_blank"
+        >
+          <img
+            className="project__img"
+            src={pickmiup}
+            alt="Project Pickmiup"
+          ></img>
           <div className="project__description">
             <h3>Food ordering app, Pickmiup</h3>
-            <span>HTML, CSS, JS, NodeJS, Express, EJS, jQuery ,PostgreSQL, Bootstrap, Sass</span>
+            <span>
+              HTML, CSS, JS, NodeJS, Express, EJS, jQuery ,PostgreSQL,
+              Bootstrap, Sass
+            </span>
           </div>
         </a>
-        <a href="https://github.com/minha0817/to-do-list" className="project"  target="_blank">
-          <img className="project__img" style={{width: 200, height: 150}} src={todolist} alt="Project Todolist"></img>
+        <a
+          href="https://github.com/minha0817/to-do-list"
+          className="project"
+          target="_blank"
+        >
+          <img
+            className="project__img"
+            src={todolist}
+            alt="Project Todolist"
+          ></img>
           <div className="project__description">
             <h3>To Do List app</h3>
             <span>HTML, CSS, JS, React</span>
