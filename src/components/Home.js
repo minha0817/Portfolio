607d21c4be1129e431054314cb214ba303cbcf00
minha0817 <minhakk24@gmail.com>
2023-01-20 19:35:09 -0800

    Update CONTACT ME button

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,22 @@ import Picture from "../pics/profile2.png";
 import "../styles/home.css";
 
 export default function Home() {
-  return(
+  return (
     <section id="home">
       <img className="home__picture" src={Picture} alt="Picture"></img>
-      <h1 className="home__title">Hello, <br/>I'm Minha Kim</h1>
-      <h3 className="home__description">A Front-end Developer currently residing in Vancouver, Canada </h3>
-      <button className="home__contact">CONTACT ME</button>
+      <h1 className="home__title">
+        Hello, <br />
+        I'm Minha Kim
+      </h1>
+      <h3 className="home__description">
+        A Front-end Developer currently residing in Vancouver, Canada{" "}
+      </h3>
+      <a
+        href="mailto:minhakk24@gmail.com"
+        target="_blank"
+      >
+        <button className="home__contact">CONTACT ME</button>
+      </a>
     </section>
-  )
+  );
 }
