a94a9088ad6849fcfa387a9642840bc1fc2a15eb
minha0817 <minhakk24@gmail.com>
2023-01-25 14:25:17 -0800

    JS : Add progress bar

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+
+export default function ProgressBar(props) {
+
+  const {bgcolor, completed} = props;
+
+  const containerStyles = {
+    height: 15,
+    width: '100%',
+    backgroundColor: "#e0e0de",
+    borderRadius: 50,
+    margin: 10,
+    flexbasis: "60%"
+  }
+
+  const fillerStyles = {
+    height: '100%',
+    width: `${completed}%`,
+    backgroundColor: bgcolor,
+    borderRadius: 'inherit',
+    textAlign: 'right',
+    transition: 'width 2s',
+  }
+
+  const labelStyles = {
+    color: 'white',
+  }
+
+
+  return (
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        {/* <span style={labelStyles}>{`${completed}%`}</span> */}
+      </div>
+    </div>
+  )
+}
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import "../components/ProgressBar";
+import ProgressBar from "../components/ProgressBar";
 import "../styles/skill.css";
 
+const skills = [
+  {
+    language: "HTML",
+    completed: 85,
+    bgcolor: "#d17163"
+  },
+  {
+    language: "CSS",
+    completed: 80,
+    bgcolor: "#d17163"
+  },
+  {
+    language: "Javascript",
+    completed: 75,
+    bgcolor: "#d17163"
+  },
+  {
+    language: "React",
+    completed: 70,
+    bgcolor: "#d17163"
+  },
+  {
+    language: "NodeJS",
+    completed: 65,
+    bgcolor: "#d17163"
+  }
+];
+
 export default function Skill() {
+
+  const [completed, setCompleted] = useState(0);
+
+  useEffect(() => {
+
+    setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 2000);
+    
+  }, []);
+
   return (
     <section id="skills" className="section">
       <h1 className="skill__title">Skills</h1>
@@ -14,36 +53,17 @@ export default function Skill() {
         <div className="skillset__left">
           <span className="skillset__title">Skills</span>
           <div className="skillset__all__left">
-            <div className="languages">
-              <div className="language">
-                <span>HTML</span>
-                <span>85%</span>
+            {skills.map((skill, index) => (
+              <div className="languages">
+                <div className="language">
+                  <span>{skill.language}</span>
+                  <span>{`${skill.completed}%`}</span>
+                </div>
+                <div>
+                  <ProgressBar key={index} bgcolor={skill.bgcolor} completed={skill.completed}/>
+                </div>
               </div>
-            </div>
-            <div className="languages">
-              <div className="language">
-                <span>CSS</span>
-                <span>70%</span>
-              </div>
-            </div>
-            <div className="languages">
-              <div className="language">
-                <span>Javascript</span>
-                <span>60%</span>
-              </div>
-            </div>
-            <div className="languages">
-              <div className="language">
-                <span>React</span>
-                <span>60%</span>
-              </div>
-            </div>
-            <div className="languages">
-              <div className="language">
-                <span>NodeJS</span>
-                <span>60%</span>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
         <div></div>
