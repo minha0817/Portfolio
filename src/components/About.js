a5cc7c2b483034f1641b903c9890a3fd2b200136
minha0817 <minhakk24@gmail.com>
2023-01-26 12:56:12 -0800

    Update About me

--- src/components/About.js
+++ src/components/About.js
@@ -1,32 +1,53 @@
 import React from "react";
-import "../styles/about.css"
-import {AiFillHtml5} from "react-icons/ai"
-import {AiFillDatabase} from "react-icons/ai"
+import "../styles/about.css";
+import { AiFillHtml5 } from "react-icons/ai";
+import { AiFillDatabase } from "react-icons/ai";
 
 export default function About() {
   return (
     <section id="about" className="section section__container">
       <h1 className="about__title">About me</h1>
-      <p className="about__description">
-        A front-end web developer transitioning from the early childhood
-        education industry. <br/>I enjoy the fast-paced, ever-changing environment of
-        web development and being able to create websites that make a difference
-        in people’s lives.
-      </p>
+      <div className="about__description">
+        <p className="about__description__detail">
+          🏡 <strong>My background was :</strong> Early Childhood Education, Biology.
+          <br />
+        </p>
+        <p className="about__description__detail"> 
+          🍎 <strong>I’m interested in :</strong> Miracle Morning, Cooking, Indoor Cycling, Coding
+          <br />
+        </p>
+        <p className="about__description__detail">
+          💻 <strong>I’m currently learning :</strong> NextJS <br />
+        </p>
+        <p className="about__description__detail">
+          🌱 <strong>I’m looking to collaborate in :</strong> Education Development <br />
+        </p>
+      </div>
 
       <ul className="about__majors">
         <li className="major major__front-end">
-          <AiFillHtml5 className="major__icon" style={{ fontSize: 150, color: "d17163"}}/>
+          <AiFillHtml5
+            className="major__icon"
+            style={{ fontSize: 150, color: "d17163" }}
+          />
           <h2 className="major__title">Front-end</h2>
-          <p className="major__description">HTML, CSS, Javascript, <br/>React, Web APIs</p>
+          <p className="major__description">
+            HTML, CSS, Javascript, <br />
+            React, Web APIs
+          </p>
         </li>
         <li className="major">
-          <AiFillDatabase className="major__icon" style={{ fontSize: 150, color: "d17163" }}/>
+          <AiFillDatabase
+            className="major__icon"
+            style={{ fontSize: 150, color: "d17163" }}
+          />
           <h2 className="major__title">Back-end</h2>
-          <p className="major__description">Javascript, NodeJS, Express, <br/>Rest APIs, Ruby on Rails, SQL </p>
+          <p className="major__description">
+            Javascript, NodeJS, Express, <br />
+            Rest APIs, Ruby on Rails, SQL{" "}
+          </p>
         </li>
       </ul>
-
     </section>
   );
 }
